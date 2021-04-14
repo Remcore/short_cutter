aa6411ddab2e1ea7dc9070b176601506bf8426cb
Egor Yuzefatov <yuzefatov@gmail.com>
2021-04-14 22:34:49 +0700

    fix validations for short_url

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,12 +2,14 @@
 
 class Url < ApplicationRecord
   validates :full_url, presence: true
+  validates :short_url, presence: true, uniqueness: true
 
-  before_save :create_short_url
+  before_validation :create_short_url
 
   def get_redis_key
     "urls:#{self.id}"
   end
+
   private
     def create_short_url
       self.short_url = SecureRandom.alphanumeric(6) if self.short_url.blank?
